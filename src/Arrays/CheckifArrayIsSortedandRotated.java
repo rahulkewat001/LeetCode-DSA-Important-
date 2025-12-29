1c2a91c147f4e7a8522488eca0db48ab24ae31f5
rahulkewat001 <rahulkewat68734@gmail.com>
2025-12-29 16:42:52 +0530

    Added CheckifArrayIsSortedandRotated solution

---
+++ src/Arrays/CheckifArrayIsSortedandRotated.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class CheckifArrayIsSortedandRotated {
+    public static boolean check(int[] nums) {
+        if(nums.length == 0) {
+            return false;
+        }
+
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > nums[(i+1) % nums.length]) {
+                count++;
+            }
+        }
+        return count <= 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,4,5,1,2};
+        System.out.println(check(nums));
+    }
+}

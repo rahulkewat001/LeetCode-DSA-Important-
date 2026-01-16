0c8df37fec80db61cac0bdd2abd12d81f44a973d
rahulkewat001 <rahulkewat68734@gmail.com>
2026-01-16 20:43:36 +0530

    Added DNASequence solution

---
+++ src/String/DNASequence.java
@@ -0,0 +1,35 @@
+package String;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DNASequence {
+    public static List<String> findRepeatedDnaSequences(String s) {
+        List<String> res = new ArrayList<>();
+        if(s.length() < 10) {
+            return res;
+        }
+
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i <= s.length() - 10; i++) {
+            String sub = s.substring(i, i+10);
+            if(map.containsKey(sub)) {
+                map.put(sub, map.get(sub) + 1);
+            } else {
+                map.put(sub, 1);
+            }
+
+            if(map.get(sub) == 2) {
+                res.add(sub);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+        System.out.println(findRepeatedDnaSequences(s).toString());
+    }
+}
